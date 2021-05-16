9b0f599fac0600363e71cb77bb2ea41dbe60a980
Sayali_Takalkar <sayali27.takalkar@gmail.com>
2021-05-16 15:31:59 +0530

    Printing Pattern

---
+++ pattern.py
@@ -0,0 +1,18 @@
+#Star k x j Pattern
+k=int(input("Enter no. of Rows: "))
+l=int(input("Enter no. of Columns: "))
+def print_my_pattern(k,l):
+    for i in range(k):
+        #print(i)
+
+        for j in range(l):
+            print("*",end="")
+        print()
+print_my_pattern(k,l)
+
+
+
+
+    
+
+
