2686d056e5cc013956ab46f7063161f7473f9d6a
Sayali_Takalkar <sayali27.takalkar@gmail.com>
2021-05-23 23:21:09 +0530

    To find area of circle

---
+++ circle.py
@@ -0,0 +1,5 @@
+#Computing the Area of Circle
+PI = 3.14
+radius = int(input("Enter radius:"))
+area = PI*radius**2
+print("Area of the circle with radius "+str(radius)+" meters is "+str(area)+" sq.meters.")
