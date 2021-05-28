21b4ea9cc52e94b19b949d9c96d1832b8207b039
Sayali_Takalkar <sayali27.takalkar@gmail.com>
2021-05-28 09:25:43 +0530

    While loop

---
+++ while_loop.py
@@ -0,0 +1,11 @@
+#7-4 while loop asking user to asking his choice of pizza toppings
+msg = ("(Enter 'done' when completed with the list.)")
+msg += ("\nEnter the pizza toppings you want on your pizza: ")
+toppings=""
+while toppings != 'done':
+    toppings = input(msg)
+
+    if toppings != 'done':
+        print(toppings+" added")
+    else:
+        print("Done with your pizza.")
