f4cf37da73990b36508c483924a7750a55c7c176
Sayali_Takalkar <sayali27.takalkar@gmail.com>
2021-07-10 20:53:25 +0530

    calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 # A Calculator
-from calci_module import*
+from calculator_module import*
 #Asking for the operation to be performed
-print("Which operation do you want to perform ?\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n 5.Area of Square\n 6.Area of Rectangle\n 7.Area of Triangle\n 8.Area of a Circle\n 9.Area of a Semi-circle\n 10.Factorial\n")
+print("Which operation do you want to perform ?\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n 5.Area of Square\n 6.Area of Rectangle\n 7.Area of Triangle\n 8.Area of a Circle\n 9.Area of a Semi-circle\n 10.Factorial\n 11.Square of the number\n 12.Square root of the number\n 13.Cube of the number\n 14.Cube root of the number\n")
 #Asking for the choice no.
 choice = int(input("Enter your choice number: "))
 print("\n")
@@ -41,6 +41,18 @@ elif choice == 9:
 elif choice == 10:
     a = int(input('Enter the number: '))
     fact(a)
+elif choice == 11:
+    a = int(input("Enter the number:"))
+    square(a)
+elif choice == 12:
+    a = int(input("Enter the number:"))
+    sq_root(a)
+elif choice == 13:
+    a = int(input("Enter the number:"))
+    cube(a)
+elif choice == 14:
+    a = int(input("Enter the number:"))
+    cube_root(a)
 
 print('\nThank You:)')
   
