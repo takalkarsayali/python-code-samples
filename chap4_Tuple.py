9bc1a3a656ff0d54d12db286f8e0b5bf360f909a
Sayali_Takalkar <sayali27.takalkar@gmail.com>
2021-07-11 21:46:43 +0530

    removed sample programs

--- chap4_Tuple.py
+++
@@ -1,9 +0,0 @@
-#4-13 Buffet items in resturant 
-#Original menu
-menu=('Icecream','Italian','Chinese','Western')
-for food in menu:
-    print(food)
-#Modified Menu
-menu=('Icecream','Italian','Chinese','Western','Indian')
-for food in menu:
-    print(food)
--- thank_you.py
+++
@@ -1,4 +0,0 @@
-#Thanking everyone for wishing on my birthday
-msg = ("Thank You all of you for your wishes and your blessings.")
-msg += ("\nStay happy , stay safe and Take Care.")
-print(msg)
